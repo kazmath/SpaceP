0bdd056fd99aafd8e77fb3f6ce99eaf072e9f0f6
Kazuma-Chan <66137568+Kazuma-chan@users.noreply.github.com>
2023-05-22 22:34:46 -0300

    Começo da refatoração

--- src/main/java/br/edu/unifacol/spacep/controller/SpacePController.java
+++ src/main/java/br/edu/unifacol/spacep/controller/SpacePController.java
@@ -1,5 +1,8 @@
 package br.edu.unifacol.spacep.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +23,17 @@ public class SpacePController {
 	public ResponseEntity<MediaDTO[]> getImagesAll() throws Exception {
 		// Astronomy Picture of the Day
 		MediaDTO[] nasaAPOD;
-		nasaAPOD = service.saveMedia("https://api.nasa.gov/planetary/apod?api_key=**API_KEY**&start_date=2023-05-12&thumbs=True");
+		String endpoint = "https://api.nasa.gov/planetary/apod";
+		String api_key = "**API_KEY**";
+		// String start_date = "2023-05-12";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String start_date = LocalDate.now().minusDays(6).format(formatter);
+		String url = endpoint + "?" +
+			"api_key="		+ api_key		+ "&" +
+			"start_date="	+ start_date	+ "&" +
+			"thumbs="		+ "True";
+	
+		nasaAPOD = service.saveMedia(url);
 		return ResponseEntity
 			.ok(nasaAPOD);
 		// return new ResponseEntity<>(nasaAPOD, HttpStatus.OK);
