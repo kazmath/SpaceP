7a16d208660342f607f505b2d4441f183cdd2948
Matheus H <66137568+kazmath@users.noreply.github.com>
2023-11-03 23:29:05 -0300

    Ajustar API para o app mobile

--- src/main/java/br/edu/unifacol/spacep/service/MediaService.java
+++ src/main/java/br/edu/unifacol/spacep/service/MediaService.java
@@ -6,7 +6,7 @@ import br.edu.unifacol.spacep.domain.Media;
 import br.edu.unifacol.spacep.dto.MediaDTO;
 
 public interface MediaService {
-	List<MediaDTO> saveMedia() throws Exception;
-	MediaDTO saveAndGetToday() throws Exception;
+	List<MediaDTO> saveMedia(String API_KEY) throws Exception;
+	MediaDTO saveAndGetToday(String API_KEY) throws Exception;
 	List<Media> getHistory();
 }
--- src/main/java/br/edu/unifacol/spacep/service/MediaServiceImpl.java
+++ src/main/java/br/edu/unifacol/spacep/service/MediaServiceImpl.java
@@ -25,16 +25,16 @@ public class MediaServiceImpl implements MediaService {
 	private MediaRepository repository; // repository.save(...)
 
 	@Override
-	public List<MediaDTO> saveMedia() throws Exception {
+	public List<MediaDTO> saveMedia(String API_KEY) throws Exception {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		String start_date = LocalDate.now().minusDays(6).format(formatter);
 		String url = Constants.BASE_URL + "?" +
-				"api_key=" + Constants.API_KEY + "&" +
+				"api_key=" + API_KEY + "&" +
 				"start_date=" + start_date + "&" +
 				"thumbs=" + "True";
 
 		ParameterizedTypeReference<ArrayList<MediaDTO>> typeRef = new ParameterizedTypeReference<ArrayList<MediaDTO>>() {};
-				
+
 		ArrayList<MediaDTO> response = new RestTemplate().exchange(RequestEntity.get(url).build(), typeRef).getBody();
 		ArrayList<MediaDTO> responseOut = new ArrayList<MediaDTO>(response);
 
@@ -55,9 +55,9 @@ public class MediaServiceImpl implements MediaService {
 	}
 
 	@Override
-	public MediaDTO saveAndGetToday() throws Exception {
+	public MediaDTO saveAndGetToday(String API_KEY) throws Exception {
 		String url = Constants.BASE_URL + "?" +
-				"api_key=" + Constants.API_KEY + "&" +
+				"api_key=" + API_KEY + "&" +
 				"thumbs=" + "True";
 
 		MediaDTO response = new RestTemplate().exchange(RequestEntity.get(url).build(), MediaDTO.class).getBody();
@@ -77,22 +77,24 @@ public class MediaServiceImpl implements MediaService {
 
 	@Override
 	public List<Media> getHistory() {
-		ArrayList<Media> output = new ArrayList<Media>();
-		
-		Stream<LocalDate> last7Days = LocalDate.now()
-			.minusDays(6)
-			.datesUntil(
-				LocalDate.now().plusDays(1)
-			);
+		List<Media> output = new ArrayList<Media>();
 
-		last7Days.iterator().forEachRemaining((day) -> {
-			// Media dayMedia = null;
-			if (repository.findByDate(day).size() != 0) {
-				output.add(repository.findByDate(day).get(0));
-			} else {
-				output.add(null);
-			}
-		});
+		output = repository.findAll();
+
+		// Stream<LocalDate> last7Days = LocalDate.now()
+		// 	.minusDays(6)
+		// 	.datesUntil(
+		// 		LocalDate.now().plusDays(1)
+		// 	);
+
+		// last7Days.iterator().forEachRemaining((day) -> {
+		// 	// Media dayMedia = null;
+		// 	if (repository.findByDate(day).size() != 0) {
+		// 		output.add(repository.findByDate(day).get(0));
+		// 	} else {
+		// 		output.add(null);
+		// 	}
+		// });
 
 		return output;
 	}
