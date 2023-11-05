5bd416845512c334451040c44ae033f8bb84c145
DanielFigueiroa <danielfigueiroa11@gmail.com>
2023-11-05 17:20:03 -0300

    Funciona

--- src/main/java/br/edu/unifacol/spacep/controller/SpacePController.java
+++ src/main/java/br/edu/unifacol/spacep/controller/SpacePController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.unifacol.spacep.domain.Media;
@@ -19,14 +20,14 @@ public class SpacePController {
 	@Autowired
 	private MediaService service;
 
-	@GetMapping("/save/{APIKEY}")
-	public ResponseEntity<List<MediaDTO>> save(@PathVariable String APIKEY) throws Exception {
+	@GetMapping("/save")
+	public ResponseEntity<List<MediaDTO>> save(@RequestParam String APIKEY) throws Exception {
 		List<MediaDTO> nasaAPOD = service.saveMedia(APIKEY);
 		return ResponseEntity.ok(nasaAPOD);
 	}
 
-	@GetMapping("/today/{APIKEY}")
-	public ResponseEntity<MediaDTO> today(@PathVariable String APIKEY) throws Exception {
+	@GetMapping("/today")
+	public ResponseEntity<MediaDTO> today(@RequestParam String APIKEY) throws Exception {
 		MediaDTO todayMedia = service.saveAndGetToday(APIKEY);
 		return ResponseEntity.ok(todayMedia);
 	}
